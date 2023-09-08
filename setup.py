446af8bf118fbf86cdf6b56ba8eec534ccfb1e2c
Xusheng Ai <xai@node1271.palmetto.clemson.edu>
2023-09-08 11:32:22 -0400

    build the project with module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "DDPM-gene-perturbation",
+    py_modules = ["ddpm"], 
+    
+)
+
+"""
+py_modules: A list of strings specifying the modules
+            that setuptools will manipulate.
+"""
